9d74775e592411cfdfb80c1d468eccc683b9161b
Administrator <tomiavikainen@gmail.com>
2024-01-17 12:05:13 +0200

    Testi

---
+++ tunti1juttuja.py
@@ -0,0 +1,17 @@
+#käyttäjä = input('Anna nimesi: ')
+#print("Hauska tavata, "+käyttäjä+"!")
+
+#eka = -9
+#toka = 12_456_123_180
+#kolmas = 4.973
+#neljäs = -4 + 2j
+
+#print (eka)
+#print (toka)
+#print (kolmas)
+#print (neljäs)
+
+fahrenheit_str = input("Anna lämpötila Fahrenheit-asteina: ")
+fahrenheit = float(fahrenheit_str)
+celsius = (fahrenheit-32)*5/9
+print("Lämpötila Celsius-asteina: " + str(celsius))
