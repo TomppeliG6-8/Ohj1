ee95f4b1f560c68903692a056cad0c94a9c0aa70
Tomi <tomiavikainen@gmail.com>
2024-01-20 18:55:16 +0200

    Toimii

--- Moduuli 3/Teht1.py
+++ Moduuli 3/Teht1.py
@@ -3,4 +3,4 @@ kp=float(input("Anna kuhan pituus: "))
 if kp>36:
     print("Kuha on täysimittainen")
 else:
-    print(f"Kuha on {37-kp}cm alamittainen.")
+    print(f"Kuha on {37-kp}cm alamittainen. Päästä se takaisin järveen")
